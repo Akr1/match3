dccc74874a026c372656223c3b82d1f0be68ff15
Ariel Kim <Arielekc@gmail.com>
2020-02-15 14:10:20 -0500

    Implement Board/Piece population

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -8,13 +8,23 @@ export (int) var x_start;
 export (int) var y_start;
 export (int) var offset;
 
+var possible_pieces = [
+preload("res://scenes/yellow_piece.tscn"),
+preload("res://scenes/blue_piece.tscn"),
+preload("res://scenes/pink_piece.tscn"),
+preload("res://scenes/orange_piece.tscn"),
+preload("res://scenes/green_piece.tscn"),
+preload("res://scenes/light_green_piece.tscn")
+];
+
 #Empty array
 var all_pieces = [];
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize();
+	all_pieces = make_2d_array();
+	spawn_pieces();
 
 func make_2d_array():
 	var array = [];
@@ -24,6 +34,22 @@ func make_2d_array():
 		for j in height:
 			array[i].append(null);
 	return array;
+
+func spawn_pieces():
+	for i in width:
+		for j in height:
+			#choose a random number and store it
+			var rand = floor(rand_range(0, possible_pieces.size()));	
+			#Instance that piece from the array
+			var piece = possible_pieces[rand].instance();
+			add_child(piece);
+			piece.position = grid_to_pixel(i, j);
+
+	
+func grid_to_pixel(column, row):
+	var new_x = x_start + offset * column;
+	var new_y = y_start + -offset * row;
+	return Vector2(new_x, new_y);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
